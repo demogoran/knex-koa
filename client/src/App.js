a091ff7196941aeed6863f9f7fde48b6310b9e7f
Anton <demogoran@gmail.com>
2020-02-23 11:16:58 +0200

    material UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { List, ListItem, ListItemText, Modal } from '@material-ui/core';
 
 import all from './tools/api';
 
@@ -8,37 +9,75 @@ import styles from './App.css';
 
 const { Serials } = all;
 
+const ListItemLink = (props) => <ListItem button component="a" {...props} />;
+
 export default () => {
   const [str, setStr] = useState('друзья');
   const [list, setList] = useState([]);
   const [seasons, setSeasons] = useState([]);
   const [series, setSeries] = useState([]);
+  const [currentVideo, setCurrentVideo] = useState([]);
+
+  const [open, setOpen] = useState(false);
 
   let currentContent = <div></div>;
 
   if (series.length) {
-    currentContent = series
-      .map(x => <a key={x.url} target="_blank" href={x.file}>{x.title.replace('<br>', '')}</a>);
+    currentContent = <List component="nav" aria-label="secondary mailbox folders">
+      {series
+        .map(x => <ListItem
+          button
+          key={x.file}
+          onClick={async () => {
+            setCurrentVideo(x.file);
+            setOpen(true);
+          }}>
+          <ListItemText primary={x.title.replace('<br>', '')} />
+        </ListItem>)}
+    </List>
   }
   else if (seasons.length) {
-    currentContent = seasons
-      .map(x => <a key={x.url} onClick={async () => {
-        const r = await Serials.series(x.url);
-        setSeries(r);
-        setSeasons([]);
-      }}>{x.name}</a>);
+    currentContent = <List component="nav" aria-label="secondary mailbox folders">
+      {seasons
+        .map(x => <ListItem
+          button
+          key={x.url}
+          onClick={async () => {
+            const r = await Serials.series(x.url);
+            setSeries(r);
+            setSeasons([]);
+          }}>
+          <ListItemText primary={x.name} />
+        </ListItem>)}
+    </List>
   }
   else if (list.length) {
-    currentContent = list
-      .map(x => <a key={x.href} onClick={async () => {
-        const r = await Serials.seasons(x.href);
-        setSeasons(r);
-        setList([]);
-      }}>{x.name}</a>);
+    currentContent = <List component="nav" aria-label="secondary mailbox folders">
+      {list
+        .map(x => <ListItem
+          button
+          key={x.href}
+          onClick={async () => {
+            const r = await Serials.seasons(x.href);
+            setSeasons(r);
+            setList([]);
+          }}>
+          <ListItemText primary={x.name} />
+        </ListItem>)}
+    </List>
   }
 
 
   return <div>
+    <Modal
+      aria-labelledby="simple-modal-title"
+      aria-describedby="simple-modal-description"
+      open={open}
+      onClose={() => setOpen(false)}
+    >
+      <video controls autoPlay className="modal" src={currentVideo}></video>
+    </Modal>
+
     <input type="text" value={str} onChange={(e) => setStr(e.target.valuee)}></input>
 
     <div onClick={async () => {
